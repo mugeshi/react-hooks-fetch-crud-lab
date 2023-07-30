41e57564d6ce02862a8be4342380caf4f016da70
Njeri Mugeshi <nicolemugeshi@gmail.com>
2023-07-30 08:10:23 +0300

    done

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,69 +1,71 @@
 import React, { useState } from "react";
 
-function QuestionForm(props) {
-  const [formData, setFormData] = useState({
-    prompt: "",
-    answer1: "",
-    answer2: "",
-    answer3: "",
-    answer4: "",
-    correctIndex: 0,
-  });
+function handleSubmit(event) {
+  const newQuestionData = {
+    prompt: formData.prompt,
+    answers: [
+      formData.answer1,
+      formData.answer2,
+      formData.answer3,
+      formData.answer4,
+    ],
+    correctIndex: formData.correctIndex,
+  };
+  event.preventDefault();
+  console.log(formData);
+  fetch(`http://localhost:4000/questions`, {
+    method: "POST",
+    body: JSON.stringify(newQuestionData),
+    headers: {
+      "Content-type": "application/json",
+    },
+  })
+    .then((resp) => resp.json())
+    .then((question) => props.onAddQuestion(question));
+}
 
-  function handleChange(event) {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log(formData);
-  }
-
-  return (
-    <section>
-      <h1>New Question</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Prompt:
-          <input
-            type="text"
-            name="prompt"
-            value={formData.prompt}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Answer 1:
-          <input
-            type="text"
-            name="answer1"
-            value={formData.answer1}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Answer 2:
-          <input
-            type="text"
-            name="answer2"
-            value={formData.answer2}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Answer 3:
-          <input
-            type="text"
-            name="answer3"
-            value={formData.answer3}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Answer 4:
+return (
+  <section>
+    <h1>New Question</h1>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Prompt:
+        <input
+          type="text"
+          name="prompt"
+          value={formData.prompt}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Answer 1:
+        <input
+          type="text"
+          name="answer1"
+          value={formData.answer1}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Answer 2:
+        <input
+          type="text"
+          name="answer2"
+          value={formData.answer2}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Answer 3:
+        <input
+          type="text"
+          name="answer3"
+          value={formData.answer3}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+      Answer 4:
           <input
             type="text"
             name="answer4"
@@ -88,6 +90,5 @@ function QuestionForm(props) {
       </form>
     </section>
   );
-}
 
 export default QuestionForm;
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,25 +1,54 @@
 import React from "react";
 
 function QuestionItem({ question }) {
-  const { id, prompt, answers, correctIndex } = question;
-
-  const options = answers.map((answer, index) => (
-    <option key={index} value={index}>
-      {answer}
-    </option>
-  ));
-
-  return (
-    <li>
-      <h4>Question {id}</h4>
-      <h5>Prompt: {prompt}</h5>
-      <label>
-        Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
-      </label>
-      <button>Delete Question</button>
-    </li>
-  );
-}
-
-export default QuestionItem;
+  function QuestionItem({ question, onDeleteQuestion, onUpdateQuestion }) {
+    const { id, prompt, answers, correctIndex } = question;
+  
+    const options = answers.map((answer, index) => (
+  function QuestionItem({ question }) 
+      {options}
+    ));
+  
+  
+    function handleUpdatingDefaultAnswer(e) {
+      fetch(`http://localhost:4000/questions/${question.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          correctIndex: Number.parseInt(e.target.value),
+        }),
+      })
+        .then((r) => r.json())
+        .then((updatedItem) => onUpdateQuestion(updatedItem));
+    }
+  
+    function handleDeleteClick() {
+      fetch(`http://localhost:4000/questions/${question.id}`, {
+        method: "DELETE",
+      })
+        .then((r) => r.json())
+        .then(() => onDeleteQuestion(question.id));
+    }
+  
+    return (
+      <li>
+        <h4>Question {id}</h4>
+        <h5>Prompt: {prompt}</h5>
+        <label>
+          Correct Answer:
+          <select defaultValue={correctIndex}>{options}</select>
+          <select
+            defaultValue={correctIndex}
+            onChange={handleUpdatingDefaultAnswer}
+          >
+            {options}
+          </select>
+        </label>
+        <button>Delete Question</button>
+        <button onClick={handleDeleteClick}>Delete Question</button>
+      </li>
+    );
+  }}
+  export default QuestionItem;
